ad6d45ac1923d9ea5fa43e555da4641093aa76b0
TusharSatija <tusharsatija737@gmai.com>
2026-01-31 09:23:51 +0000

    update

---
+++ AB/Lecture-10/app.js
@@ -0,0 +1,21 @@
+let express=require('express');
+let app=express();
+let path=require('path');
+let bodyparser=require('body-parser');
+app.set("view engine",'ejs')
+app.set('views',path.join(__dirname,'views'));
+app.use(bodyparser.urlencoded({extended:true}))
+
+
+app.get('/register',(req,res)=>{
+    res.render('index');
+})
+
+app.post('/register',(req,res)=>{
+    console.log(req.body);
+    res.render("user register successfully....")
+})
+
+app.listen(3000,()=>{
+    console.log("app is running at port 3000")
+})
