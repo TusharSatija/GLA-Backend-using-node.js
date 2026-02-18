90ead2688317ecd9eba8b2fc37f3afa2971b5f37
TusharSatija <tusharsatija737@gmai.com>
2026-02-18 11:23:43 +0530

    update

---
+++ Q/Lecture-15/condition.sh
@@ -0,0 +1,10 @@
+echo "enter num";
+read n
+
+if [ $n -gt 20 ]
+   then
+   echo "enter is greater than 20";
+   else
+   echo "enter num is lesser than 20";
+   fi;
+
---
+++ Q/Lecture-15/hello.sh
@@ -0,0 +1,5 @@
+#this is a comment
+
+echo "hello using script shell"
+
+read n
