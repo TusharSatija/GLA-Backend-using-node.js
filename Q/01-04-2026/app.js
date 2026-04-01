6a940e8390f412c3229c4558ec2fee4525244caa
TusharSatija <tusharsatija737@gmail.com>
2026-04-01 10:53:52 +0530

    mongo

--- Q/01-04-2026/app.js
+++ Q/01-04-2026/app.js
@@ -14,11 +14,15 @@ mongoose.connect("mongodb://127.0.0.1:27017/3Q")
 
 async function insert(data)
 {
-    let u1= new User(data);
-     await u1.save(); 
+    // let u1= new User(data);
+    //  await u1.save(); 
+
+    await User.insertMany(data)
      console.log("inserted !!");  
 }
-insert({username:"rohan",password:"secret"});
+insert([{username:"abc",password:"secret"},
+    {username:"Rahul",password:"thisisSecret"}
+]);
 
 
 
