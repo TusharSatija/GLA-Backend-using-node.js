6c66dcf08ae13234fafd47ca4ad59acd70c3cca9
TusharSatija <tusharsatija737@gmail.com>
2026-04-06 15:05:02 +0530

    update

---
+++ AB/Lecture-29/middleware.js
@@ -0,0 +1,23 @@
+let express=require('express');
+let app=express();
+
+app.use((req,res,next)=>{
+    console.log('first middleware');
+    next();
+    console.log('first middleware after next');
+});
+
+app.use((req,res,next)=>{
+    console.log('second middleware');
+    // next();
+    console.log('second middleware after next');
+});
+
+app.get('/',(req,res,next)=>{
+    res.send("this is a / route ");
+    next();
+})
+
+app.listen(3000,()=>{
+    console.log("app is running at port 3000")
+})
